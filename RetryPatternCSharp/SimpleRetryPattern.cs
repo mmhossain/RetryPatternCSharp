5f74ac876c907deed4cf34637a8bfd5c4e9ce633
mmhossain <mosharaf.hossain221@gmail.com>
2024-10-02 11:40:37 -0700

    Add project files.

---
+++ RetryPatternCSharp/SimpleRetryPattern.cs
@@ -0,0 +1,43 @@
+﻿namespace RetryPatternCSharp;
+
+public class SimpleRetryPattern
+{
+    // This is to simulate logging, which it helps with debug and test.
+    public Dictionary<int, string> LogMessages = [];
+
+    public void Execute()
+    {
+        executeWithRetry(unreliableServiceCall, 3, 5);
+    }
+
+    private void executeWithRetry(Action operation, int maxRetries, int delayInSeconds)
+    {
+        int retryCount = 0;
+
+        while (retryCount < maxRetries) // retry a limited number of times
+        {
+            try
+            {
+                operation?.Invoke();
+                return;
+            }
+            catch (Exception ex)
+            {
+                retryCount++;
+                LogMessages[retryCount] = $"Attempt {retryCount} failed with message: {ex.Message}";
+
+                Thread.Sleep(delayInSeconds * 1000);    // wait between retries
+            }
+        }
+
+        LogMessages[maxRetries] = "All attempts failed!";
+    }
+
+    private void unreliableServiceCall()
+    {
+        Random rnd = new Random();
+
+        if (rnd.Next(10) < 7)
+            throw new Exception("Service unavailable!");
+    }
+}
---
+++ RetryPatternCSharpTests/SimpleRetryPatternTests.cs
@@ -0,0 +1,19 @@
+﻿namespace RetryPatternCSharp.Tests;
+
+[TestClass]
+public class SimpleRetryPatternTests
+{
+    [TestMethod]
+    public void ExecuteTest()
+    {
+        // Arrange
+        SimpleRetryPattern rp = new SimpleRetryPattern();
+
+        // Act
+        for (int i = 0; i < 10; i++)
+            rp.Execute();
+
+        // Assert
+        Assert.IsTrue(rp.LogMessages.Count > 0);
+    }
+}
