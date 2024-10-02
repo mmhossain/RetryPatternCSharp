0c35103a3d3bb37b200ddbc37316a8ed454de981
mmhossain <mosharaf.hossain221@gmail.com>
2024-10-02 14:16:55 -0700

    Use retry library Polly

---
+++ RetryPatternCSharp/RetryUsingPolly.cs
@@ -0,0 +1,44 @@
+﻿using Polly;
+
+namespace RetryPatternCSharp;
+
+public class RetryUsingPolly
+{
+    public Dictionary<int, string> LogMessages = [];
+    private int totalRetries = 0;
+    private Policy _policy;
+
+    public RetryUsingPolly(int maxRetries, int delayInSeconds)
+    {
+        _policy = Policy
+            .Handle<Exception>()
+            .WaitAndRetry(
+                retryCount: maxRetries,
+                sleepDurationProvider: attempt => TimeSpan.FromSeconds(delayInSeconds),
+                onRetry: (exception, timespan, retryCount, context) => {
+                    totalRetries++;
+                LogMessages[retryCount] = 
+                    $"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.";
+            });
+    }
+
+    public void Execute()
+    {
+        try
+        {
+            _policy.Execute(unreliableServiceCall);
+        }
+        catch (Exception Ex)
+        {
+            LogMessages[totalRetries] = $"Operation failed after {totalRetries} tries, Ex: {Ex.Message}";
+        }
+    }
+
+    private void unreliableServiceCall()
+    {
+        Random rnd = new Random();
+
+        if (rnd.Next(10) < 5)
+            throw new Exception("Service unavailable!");
+    }
+}
---
+++ RetryPatternCSharpTests/RetryUsingPollyTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RetryPatternCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetryPatternCSharp.Tests;
+
+[TestClass]
+public class RetryUsingPollyTests
+{
+    [TestMethod]
+    public void ExecuteTest()
+    {
+        // Arrange
+        RetryUsingPolly rp = new RetryUsingPolly(3, 1);
+
+        // Act
+        for (int i = 0; i < 10; i++)
+            rp.Execute();
+
+        // Assert
+        Assert.IsTrue(rp.LogMessages.Count > 0);
+    }
+}
